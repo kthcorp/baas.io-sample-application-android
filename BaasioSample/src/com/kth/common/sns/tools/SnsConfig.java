1b70939f7c2f9ead78b1c7e2d23b96d2d6e6b620
Brad Hong <breadval@hotmail.com>
2012-11-20 19:20:07 +0900

    Update BaasioSample/src/com/kth/common/sns/tools/SnsConfig.java

--- BaasioSample/src/com/kth/common/sns/tools/SnsConfig.java
+++ BaasioSample/src/com/kth/common/sns/tools/SnsConfig.java
@@ -1,16 +1,13 @@
-
 package com.kth.common.sns.tools;
 
 import com.kth.baasio.baassample.BuildConfig;
 
 public class SnsConfig {
     // Facebook App ID
-    // 510355658975579 : BaasSample_KTH(kth-store), 475255092514703 :
-    // BaasSample(DEBUG)
-    public static final String FACEBOOK_APPID = BuildConfig.DEBUG ? "510355658975579"
-            : "475255092514703"; // BaasSample
+    public static final String FACEBOOK_APPID = BuildConfig.DEBUG ? "FACEBOOK_APPID_DEBUG"
+            : "FACEBOOK_APPID"; // BaasSample
 
-    // public static final String FACEBOOK_APPID = "475255092514703";
+    // public static final String FACEBOOK_APPID = "FACEBOOK_APPID";
     // //BaasSample_KTH
 
     // Facebook Login 시 request Permissions, Permissions의 속성에 대해서는 Facebook
@@ -20,9 +17,9 @@ public class SnsConfig {
     };
 
     // Twitter Consumer, ConsumerSecret Key
-    public static final String TWITTER_CONSUMER_KEY = "S4Z3E7kzojfk2fBiyM1IMA";
+    public static final String TWITTER_CONSUMER_KEY = "TWITTER_CONSUMER_KEY";
 
-    public static final String TWITTER_CONSUMER_SECRET = "wm1HDjYUjpyI3HKjJ88AbKi0oElDjTBX1aESn9I4";
+    public static final String TWITTER_CONSUMER_SECRET = "TWITTER_CONSUMER_SECRET";
 
-    public static final String TWITTER_CALLBACK_URL = "http://testbrad.com";
+    public static final String TWITTER_CALLBACK_URL = "http://callback.url";
 }
